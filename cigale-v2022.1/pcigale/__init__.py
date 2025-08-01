e8dd11504184207eaa27f55d1ca125e5691d69e6
lisieckik <lisiecki.christopher@gmx.com>
2025-08-01 15:59:54 +0200

    Typo

--- cigale-v2022.1/pcigale/__init__.py
+++ cigale-v2022.1/pcigale/__init__.py
@@ -53,7 +53,7 @@ def check(config):
     """This part allows to change int into list of random models"""
     configuration = config.configuration
 
-    if configuration['sed_modules'][0].split('_')[0] == 'sfhstochastic':
+    if configuration['sed_modules'][0].split('_')[0] == 'sfhstohastic':
         configuration['sed_modules_params'][configuration['sed_modules'][0]]['nModels'] = (
             list(np.arange(configuration['sed_modules_params'][configuration['sed_modules'][0]]['nModels'][0])))
 
@@ -71,7 +71,7 @@ def run(config):
     """This part allows to change int into list of random models
        Used only for nonParametric sed modules"""
     configuration = config.configuration
-    if configuration['sed_modules'][0].split('_')[0] == 'sfhstochastic':
+    if configuration['sed_modules'][0].split('_')[0] == 'sfhstohastic':
         configuration['sed_modules_params'][configuration['sed_modules'][0]]['nModels'] = (
             list(np.arange(configuration['sed_modules_params'][configuration['sed_modules'][0]]['nModels'][0])))
 
